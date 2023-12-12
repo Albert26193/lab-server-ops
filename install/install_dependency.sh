8cdbc12dd77cf7fccf1fe50d4f11ecd0189ac76b
Albert.Wang <albert990624@gmail.com>
2023-12-13 00:02:33 +0800

    feat: add install dependency check

--- install/install_dependency.sh
+++ install/install_dependency.sh
@@ -5,8 +5,25 @@
 #       input: none
 #      return: 0: success | 1: fail
 ###################################################
+function lso_install_dependency() {
+    # load config file
+    local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
+    local util_file_path="${git_root}/utils/utils.sh"
 
-function pre_install {
+    if [[ ! -f "${util_file_path}" ]]; then
+        printf "%s\n" "${util_file_path} do not exist."
+        printf "%s\n" "Exit Now..."
+        return 1
+    else
+        source "${util_file_path}"
+    fi
+
+    # Check if the script is executed as root
+    if [[ "$(id -u)" -ne 0 ]]; then
+        lso_print_error_line "Please run this script as root." >&2
+        return 1
+    fi
+
     local common_install=(
         "tmux"
         "rsync"
@@ -19,7 +36,7 @@ function pre_install {
         "tree"
     )
 
-    local debian12_install=(
+    local debian_ubuntu_install=(
         "fd-find"
         "fzf"
         "duf"
@@ -27,34 +44,78 @@ function pre_install {
         "bat"
         "neovim"
         "xclip"
-        "jq"
-        "yq"
         "exa"
-        "ripgrep"
     )
 
-    local ubuntu_to_install_list=(
-        "fd-find"
+    local mac_install=(
+        "fd"
         "fzf"
         "duf"
-        "tmux"
         "neofetch"
         "bat"
-        "xclip"
-        "jq"
-        "yq"
+        "neovim"
         "exa"
-        "ripgrep"
     )
 
-    for package in "${to_install_list[@]}"; do
+    local current_os="$(lso_check_os)"
+    local all_install_list=()
+    # TODO: add more os to test
+    if [[ "${current_os}" == "macOS" ]]; then
+        all_install_list=("${common_install[@]} ${mac_install[@]}")
+    elif [[ ${current_os} == "Debian" ]]; then
+        all_install_list=("${common_install[@]}" "${debian_ubuntu_install[@]}")
+    elif [[ ${current_os} == "Ubuntu" ]]; then
+        all_install_list=("${common_install[@]}" "${debian_ubuntu_install[@]}")
+    else
+        all_install_list=("${common_install[@]}")
+    fi
+
+    local to_install_list=()
+    for package in "${all_install_list[@]}"; do
         if ! dpkg -l | grep -q "^ii[ ]* ${package}"; then
-            printf '%s\n' "${package} is not installed. Installing..."
-            sudo apt-get update && sudo apt-get install -y "${package}"
+            lso_print_red "[ X ]"
+            lso_print_red "${package}"
+            lso_print_white_line "is not installed"
+            to_install_list+=("${package}")
         else
-            printf '%s\n' "${package} is already installed."
+            lso_print_green "[ √ ]"
+            lso_print_blue "${package}"
+            lso_print_white_line "is already installed."
         fi
     done
+
+    # if all dependency installed, exit now
+    if [[ ${#to_install_list[@]} -eq 0 ]]; then
+        lso_print_green_line "All dependency installed, exit now..."
+        return 0
+    fi
+
+    # if have dependency not installed, install it
+    printf "\n"
+    lso_print_yellow "🔧Here is the list of packages to install: "
+    printf "${LSO_COLOR_CYAN}%s${LSO_COLOR_RESET} " "${to_install_list[@]}"
+    printf "\n"
+    lso_print_cyan_line "total count to install: ${#to_install_list[@]}"
+    if lso_yn_prompt "Do you want to ${LSO_COLOR_GREEN}install all dependency${LSO_COLOR_RESET}?"; then
+        if [[ "${current_os}" == "macOS" ]]; then
+            lso_print_white_line "install dependency for macOS..."
+            brew install "${to_install_list[@]}"
+        elif [[ ${current_os} == "Debian" ]]; then
+            lso_print_white_line "install dependency for Debian..."
+            apt-get update
+            apt-get install -y "${to_install_list[@]}"
+        elif [[ ${current_os} == "Ubuntu" ]]; then
+            lso_print_white_line "install dependency for Ubuntu..."
+            apt-get update
+            apt-get install -y "${to_install_list[@]}"
+        else
+            lso_print_yellow_line "not support now, exit now..."
+            return 1
+        fi
+    else
+        lso_print_white_line "do not install dependency, exit now..."
+        return 1
+    fi
 }
 
-pre_install
+lso_install_dependency
