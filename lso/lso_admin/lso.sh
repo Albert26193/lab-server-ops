9e39ee7ff81e133a2f9d08f07f77aca053e575c4
Albert.Wang <albert990624@gmail.com>
2024-01-17 21:19:36 +0800

    fix(admin): fix output style in bash

--- lso/lso_admin/lso.sh
+++ lso/lso_admin/lso.sh
@@ -20,27 +20,27 @@ function lso_admin() {
     local file_addzsh="${admin_dir}/script_addzsh/main.sh"
 
     if [[ $1 == "help" ]]; then
-        printf "---------------------------------------------------\n"
-        printf "lso_admin help:\n"
-        printf "---------------------------------------------------\n"
-        printf "   \033[32m lso_admin useradd \033[0m: add new user step by step\n"
-        printf "   \033[32m lso_admin addzsh\033[0m  : add zsh for existed user\n"
-        printf "---------------------------------------------------\n"
+        echo -e "---------------------------------------------------\n"
+        echo -e "lso_admin help:\n"
+        echo -e "---------------------------------------------------\n"
+        echo -e "   \033[32m lso_admin useradd \033[0m: add new user step by step\n"
+        echo -e "   \033[32m lso_admin addzsh\033[0m  : add zsh for existed user\n"
+        echo -e "---------------------------------------------------\n"
     elif [[ $1 == "useradd" ]]; then
         sudo bash "${file_useradd}"
     elif [[ $1 == "addzsh" ]]; then
         sudo bash "${file_addzsh}"
     elif [[ -z $1 ]]; then
-        printf "---------------------------------------------------\n"
-        printf "choose one of the following commands:\n"
-        printf "---------------------------------------------------\n"
-        printf "  \033[32m [1] lso_admin useradd \033[0m: add new user step by step\n"
-        printf "  \033[32m [2] lso_admin addzsh\033[0m  : add zsh for existed user\n"
-        printf "---------------------------------------------------\n"
+        echo -e "---------------------------------------------------\n"
+        echo -e "choose one of the following commands:\n"
+        echo -e "---------------------------------------------------\n"
+        echo -e "  \033[32m [1] lso_admin useradd \033[0m: add new user step by step\n"
+        echo -e "  \033[32m [2] lso_admin addzsh\033[0m  : add zsh for existed user\n"
+        echo -e "---------------------------------------------------\n"
 
         while true; do
-            printf "\033[36minput number of the command to execute\033[0m (1-2, default 1):\n"
-            printf "(input q to quit)\n"
+            echo -e "\033[36minput number of the command to execute\033[0m (1-2, default 1):\n"
+            echo -e "(input q to quit)\n"
             local input_number="1"
             read -r input_number
             case $input_number in
@@ -53,16 +53,16 @@ function lso_admin() {
                 break
                 ;;
             q)
-                printf "\033[32m Exit Now...\033[0m\n"
+                echo -e "\033[32m Exit Now...\033[0m\n"
                 break
                 ;;
             *)
-                printf "\033[33m ${input_number} is not valid, input again\033[0m\n"
+                echo -e "\033[33m ${input_number} is not valid, input again\033[0m\n"
                 ;;
             esac
         done
     else
-        printf "\033[31m lso_admin: command not found: $1\033[0m\n"
-        printf "\033[32m lso_admin help\033[0m: show help \n"
+        echo -e "\033[31m lso_admin: command not found: $1\033[0m\n"
+        echo -e "\033[32m lso_admin help\033[0m: show help \n"
     fi
 }
